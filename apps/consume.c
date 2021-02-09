5f7037395c9325248dad0ce43455964e061ddd73
josoell <josoell@iu.edu>
2021-02-08 19:13:28 -0600

    remove debugging prints

--- apps/consume.c
+++ apps/consume.c
@@ -5,10 +5,10 @@ void consumer(int count) {
 	// reads the value of the global variable 'n'
 	// 'count' times.
 	// print consumed value e.g. consumed : 8
-	printf("consumer: before wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
+	//printf("consumer: before wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
 	wait(can_read);
 	//wait(mutex);
-	printf("consumer: after wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
+	//printf("consumer: after wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
 	for(int i = 0; i < count; i++) {
 		printf("consumed: %d\n", n);
 	}
--- apps/produce.c
+++ apps/produce.c
@@ -6,10 +6,10 @@ void producer(int count) {
     // the value of the global variable 'n'
     // each time.
     //print produced value e.g. produced : 8
-	printf("producer: before wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
+	//printf("producer: before wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
 	wait(can_write);
 	//wait(mutex);
-	printf("producer: after wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
+	//printf("producer: after wait, can_write: %d, can_read: %d\n", semcount(can_write), semcount(can_read));
 	for(int i = 0; i < count; i++) {
 		n = i;
 		printf("produced: %d\n", i);
